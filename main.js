1ec1647ae11cf3dda3c2aa57e0691eb89aaf1854
freyamurray <freyaclmurray@gmail.com>
2025-07-18 11:26:29 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function themeSwitch() {
+  var element = document.body;
+  element.classList.toggle("dark");
+} 
+
+function emailFreya() {
+    window.location.href = "mailto:freyaclmurray@gmail.com"
+}
+
